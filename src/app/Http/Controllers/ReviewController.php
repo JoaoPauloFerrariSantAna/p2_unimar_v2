dcb8596e50364997b48713cf07543042d00e9d35
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-06-01 19:47:35 -0300

    style: reduced lines

--- src/app/Http/Controllers/ReviewController.php
+++ src/app/Http/Controllers/ReviewController.php
@@ -5,69 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Models\ReviewModel;
+use App\Http\Requests\ReviewStoreRequest;
+use App\Http\Requests\ReviewUpdateRequest;
 
 class ReviewController extends Controller
 {
-	public function getReviews()
-	{
+	public function getReviews() {
 		return response()->json(ReviewModel::all());
 	}
 
-	public function detailReview(int $id)
-	{
+	public function detailReview(int $id) {
 		$review = null;
-		try
-		{
+		try {
 			$review = ReviewModel::findOrFail($id);
-		}
-		catch(ModelNotFoundException $e)
-		{
+		} catch(ModelNotFoundException $e) {
 			return response()->json("REVIEW NOT FOUND");
 		}
 		return response()->json($review);
 	}
 
-	public function storeReview(Request $req)
-	{
-		$review = new ReviewModel();
-		$review->score = $req->input("rscore", null);
-		$review->review = $req->input("rreview", null);
-		$review->save();
-		return response()->json($review);
+	public function storeReview(ReviewStoreRequest $req) {
+		$data = $req->validaded();
+		$review = ReviewModel::create($data);
+		return response()->json($user);
 	}
 
-	public function updateReview(Request $req)
-	{
-		$review		= null;
-		$newScore	= $req->input("rscore", null);
-		$newReview	= $req->input("rreview", null);
-		try
-		{
+	public function updateReview(ReviewUpdateRequest $req) {
+		$data = $req->validated();
+		$review = null;
+		try {
 			$review = ReviewModel::findOrFail($id);
-		}
-		catch(ModelNotFoundException $e)
-		{
+		} catch(ModelNotFoundException $e) {
 			return response()->json("REVIEW NOT FOUND");
 		}
-		if($newScore != null) {
-			$review->score = $newScore;
-		}
-		if($newReview != null) {
-			$review->review = $newReview;
-		}
-		$review->save();
+		$review->update($data);
 		return response()->json($review);
 	}
 
-	public function deleteReview(int $id)
-	{
+	public function deleteReview(int $id) {
 		$review = null;
-		try
-		{
+		try {
 			$review = ReviewModel::findOrFail($id);
-		}
-		catch(ModelNotFoundException $e)
-		{
+		} catch(ModelNotFoundException $e) {
 			return response()->json("REVIEW NOT FOUND");
 		}
 		$review->delete();
