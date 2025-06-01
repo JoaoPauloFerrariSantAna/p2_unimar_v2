c2d80015bb1976e53053bb8d499d59a64069cb3c
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-06-01 17:43:26 -0300

    feat: added more two constants

--- src/constants/data_sizes.php
+++ src/constants/data_sizes.php
@@ -3,6 +3,8 @@
 define("FIELD_SIZE_DESCRIPTION", 128);
 
 // this is for generic information (names, email)
+define("FIELD_SIZE_DEFAULT_SCORE_MIN", 0);
+define("FIELD_SIZE_DEFAULT_SCORE_MAX", 5);
 define("FIELD_SIZE_DEFAULT_MIN", 1);
 define("FIELD_SIZE_DEFAULT_MAX", 32);
 
