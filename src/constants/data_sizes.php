d3c71bd353a4ba7c3650b1ef12614ae1c45ece36
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-06-01 11:37:36 -0300

    feat: added new constant and removed enum

---
+++ src/constants/data_sizes.php
@@ -0,0 +1,11 @@
+<?php
+
+define("FIELD_SIZE_DESCRIPTION", 128);
+
+// this is for generic information (names, email)
+define("FIELD_SIZE_DEFAULT_MIN", 1);
+define("FIELD_SIZE_DEFAULT", 32);
+
+define("FIELD_SIZE_PASSWD_MIN", 4);
+define("FIELD_SIZE_PASSWD_MAX", 16);
+
