f5bb66296570027a5b031eba1fbb86ca520e1d20
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-06-01 16:17:03 -0300

    feat: updated (for the last time, i hope) laravel version

---
+++ src/app/Models/ReviewModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReviewModel extends Model
+{
+	protected $table = "review_tbl";
+    protected $fillable = array("score", "review");
+}
