81f60f48a952ce73277f6c887fdfe50bc8782764
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-06-01 17:32:07 -0300

    feat: completed all two modifications on UserController

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -5,86 +5,49 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Models\UserModel;
+use App\Http\Requests\UserStoreRequest;
+use App\Http\Requests\UserUpdateRequest;
 
-class UserController extends Controller
-{
-	public function getUsers()
-	{
+class UserController extends Controller {
+	public function getUsers() {
 		return response()->json(UserModel::all());
 	}
 
-	public function detailUser(int $id)
-	{
+	public function detailUser(int $id) {
 		$user = null;
-
-		try
-		{
+		try {
 			$user = UserModel::findOrFail($id);
-		}
-		catch(ModelNotFoundException $e)
-		{
+		} catch(ModelNotFoundException $e) {
 			return response()->json("USER NOT FOUND");
 		}
-
 		return response()->json($user);
 	}
 
-	public function storeUser(Request $req)
-	{
-		$user			= new users();
-		$user->username = $req->input("usrn", null);
-		$user->email	= $req->input("usre");
-		$user->passwd	= $req->input("usrp", null);
-		$user->save();
+	public function storeUser(UserStoreRequest $req) {
+		$data = $req->validaded();
+		$user = UserModel::create($data);
 		return response()->json($user);
 	}
 
-	public function updateUser(Request $req, int $id)
-	{
-		$user		= null;
-		$newName	= $req->input("usrn", null);
-		$newEmail	= $req->input("usre", null);
-		$newPasswd	= $req->input("usrp", null);
-
-		try
-		{
-			$user = UserModel::findOrFail($id);
-		}
-		catch(ModelNotFoundException $e)
-		{
-			return response()->json("USER NOT FOUND");
-		}
-
-		if($newName != null) {
-			$user->username = $newName;
-		}
-
-		if($newEmail != null) {
-			$user->email = $newEmail;
-		}
-
-		if($newPasswd != null) {
-			$user->passwd = $newPasswd;
-		}
-
-		$user->save();
-
-		return response()->json($user);
-	}
-
-	public function deleteUser(int $id)
-	{
+	public function updateUser(UserUpdateRequest $req, int $id) {
+		$data = $req->validated();
 		$user = null;
-
-		try
-		{
+		try {
 			$user = UserModel::findOrFail($id);
-		}
-		catch(ModelNotFoundException $e)
-		{
+		} catch(ModelNotFoundException $e) {
 			return response()->json("USER NOT FOUND");
 		}
+		$user->update($data);
+		return response()->json($user);
+	}
 
+	public function deleteUser(int $id) {
+		$user = null;
+		try {
+			$user = UserModel::findOrFail($id);
+		} catch(ModelNotFoundException $e) {
+			return response()->json("USER NOT FOUND");
+		}
 		$user->delete();
 	}
 }
